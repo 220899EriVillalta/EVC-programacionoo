fccb317aa24068fb8dcf7dcaa468b7f6cf37807a
Alumnos <Alumnos@LAB_SW-PC05>
2016-10-03 08:37:30 -0500

    Pobando Ejemplo Herencia del Proyecto

---
+++ EjemploHerenci/src/ejemploherenci/EjemploHerenci.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejemploherenci;
+
+/**
+ *
+ * @author Alumnos
+ */
+public class EjemploHerenci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
